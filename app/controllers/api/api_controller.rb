e75c2603c0ce5d49ab06e022258f675fcf8c457d
AustinLinares <linaresaustin@hotmail.com>
2022-02-03 23:35:06 -0500

    token auth almost done

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,9 +1,27 @@
 module Api
   class ApiController < ActionController::API
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
-
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+    before_action :authorize_token
+    
     def not_found(error)
       render json: { errors: error.message }, status: :not_found
     end
+
+    def authorize_token
+      authenticate_token || not_authorized
+    end
+
+    def authenticate_token
+      authenticate_with_http_token do |token, _options|
+        User.find_by(token: token)
+      end
+    end
+
+    def not_authorized
+      render json: { errors: "Invalid token, please log in" }, status: :unauthorized
+    end
+    
+
   end
 end
