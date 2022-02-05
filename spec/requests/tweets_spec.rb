a1b5805653f484c882cde308c563fffb91615d5a
AustinLinares <linaresaustin@hotmail.com>
2022-02-05 12:21:05 -0500

    some tests created

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
-describe 'Genres', type: :request do
+describe 'Tweets', type: :request do
   it 'index path respond with http success status code' do
-    get '/api/genres'
-    expect(response.status).to eq(200)
+    get '/api/tweets'
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'index path returns a json with all tweets' do
+    user = User.create( username: "@probino", name: "probino", email: "probino@mail.com", password: "letmein" )
+    Tweet.create(body: 'Test', user_id: user.id)
+    get '/api/tweets'
+    tweets = JSON.parse(response.body)
+    expect(tweets.size).to eq 1
   end
 end
