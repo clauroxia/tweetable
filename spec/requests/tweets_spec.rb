200a3fb726a181ba1643b26ca5f1d216cfe5668e
AustinLinares <linaresaustin@hotmail.com>
2022-02-05 12:44:03 -0500

    some test added

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -1,15 +1,39 @@
 require 'rails_helper'
 describe 'Tweets', type: :request do
-  it 'index path respond with http success status code' do
-    get '/api/tweets'
-    expect(response).to have_http_status(:ok)
+  describe "index path" do
+    it 'respond with http success status code' do
+      get '/api/tweets'
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a json with all tweets' do
+      user = User.create( username: "@probino", name: "probino", email: "probino@mail.com", password: "letmein" )
+      Tweet.create(body: 'Test', user_id: user.id)
+      get '/api/tweets'
+      tweets = JSON.parse(response.body)
+      expect(tweets.size).to eq 1
+    end
   end
 
-  it 'index path returns a json with all tweets' do
-    user = User.create( username: "@probino", name: "probino", email: "probino@mail.com", password: "letmein" )
-    Tweet.create(body: 'Test', user_id: user.id)
-    get '/api/tweets'
-    tweets = JSON.parse(response.body)
-    expect(tweets.size).to eq 1
+  describe "show path" do
+    it "respond with http success status code" do
+      user = User.create( username: "@probino", name: "probino", email: "probino@mail.com", password: "letmein" )
+      tweet = Tweet.create(body: 'Test', user_id: user.id)
+      get api_tweet_path(tweet)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "respond with the correct tweet" do
+      user = User.create( username: "@probino", name: "probino", email: "probino@mail.com", password: "letmein" )
+      tweet = Tweet.create(body: 'Test', user_id: user.id)
+      get api_tweet_path(tweet)
+      actual_tweet = JSON.parse(response.body)
+      expect(actual_tweet["id"]).to eql(tweet.id)
+    end
+
+    # it "returns http status not found" do
+    #   get "/api/genres/:id", params: { id: "xxx" }
+    #   expect(response).to have_http_status(:not_found)
+    # end
   end
 end
